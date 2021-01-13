9b73e9e4db0b4183bce073fd90c8077219534970
Vicente Lobos <vicentelobosg@gmail.com>
2021-01-13 16:37:33 -0300

    Modelos KPI, resultado y camara agregados

--- appWebBlueLatam/camaras/models.py
+++ appWebBlueLatam/camaras/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.model import User
 
 # Create your models here.
+
+class Camara(models.Model):
+	area = models.CharField(max_length=30)
+	descripcion = models.TextField(max_length=200)
+	foto = models.ImageField()
+	usuario_fk = models.ForeignKey(User, on_delete=models.CASCADE)
+	#IP o conexion con OBS
+
+class KPI(models.Model):
+	nombre_kpi = models.CharField(max_length=30)
+	descripcion = models.TextField(max_length=200)
+	kpi = models.IntegerField()
+
+class Resultado(models.Model):
+	resultado_json = models.JSONField()
+	fecha = models.DateTimeField()
+	camara_fk = models.ForeignKey(Camara)
